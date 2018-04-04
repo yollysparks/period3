055baf99c7752668c76f341b5a90adbc4fcbf80c
yollysparks <yolanda.owiso.yo@gmail.com>
2018-04-04 10:01:04 +0200

    app.js changes

--- ex1/app.js
+++ ex1/app.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
+const gameArea =require("./gameData").gameArea.geometry;
+const players = require("./gameData").gameArea.players;
 app.get('/', (req, res) => res.send('Geo Demo!'))
+app.get("/geoapi/isuserinarea/:lon/:lat",(req,res)=>{})
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
